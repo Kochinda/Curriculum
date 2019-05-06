424fbfff9ec431890a48d97524cfd3ffff55534e
obscure723 <obscure723@yone-lab.com>
2019-05-07 06:27:47 +0900

    no message

---
+++ JavaScript/02_DOM/Practice/01/app.js
@@ -0,0 +1,47 @@
+// Q1
+let text1 = document.getElementById('text1');
+console.log(text1.textContent);
+
+//Q2
+let text2 = document.getElementById('text2');
+text2.textContent = "変更しました!!!";
+
+// Q3
+let text3 = document.getElementById('text3');
+text3.style.color = "red";
+
+// Q4
+let textGroup1 = document.getElementsByClassName('textGroup1');
+for (let i = 0; i < textGroup1.length; i++) {
+  textGroup1.item(i).style.color = "red";
+}
+
+//Q5
+let textGroup2 = document.getElementsByClassName('textGroup2');
+for (let i = 0; i < textGroup2.length; i++) {
+  textGroup2.item(i).style.backgroundColor = "pink";
+}
+
+//Q6
+let aList = document.getElementsByTagName('a');
+for (let i = 0; i < aList.length; i++) {
+  let a = aList.item(i);
+  
+  if (a.textContent == 'Google') {
+    a.href = 'https://www.google.com';
+  } else if (a.textContent == 'YouTube') {
+    a.href = 'https://www.youtube.com';
+  } else if (a.textContent == 'Yahoo') {
+    a.href = 'https://www.yahoo.co.jp/';
+  }
+}
+
+//Q7
+let text4 = document.querySelector('#text4');
+text4.style.fontSize = '35px';
+
+//Q8
+let textGroup3 = document.querySelectorAll('.textGroup3');
+for (text of textGroup3) {
+  text.style.backgroundColor = 'skyblue';
+}
