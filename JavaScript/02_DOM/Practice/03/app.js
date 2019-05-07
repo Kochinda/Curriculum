34e7a825ded33f3033f28f2b8c29c1e8cc0fe9f6
obscure723 <obscure723@yone-lab.com>
2019-05-08 06:07:06 +0900

    no message

--- JavaScript/02_DOM/Practice/03/app.js
+++ JavaScript/02_DOM/Practice/03/app.js
@@ -1,11 +1,10 @@
 document.getElementById('addBtn').addEventListener('click', function() {
   //入力内容取得
   let task = document.getElementById('task');
-  let text = document.createTextNode(task.value);
 
   // liを作成
   let li = document.createElement('li');
-  li.appendChild(text);
+  li.textContent = task.value;
   
   // リストに作成したliを追加
   let list = document.getElementById('list');
